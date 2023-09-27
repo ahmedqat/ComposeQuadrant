eb2f6a6e0fbe6b5b540d298e431b148eb98a897f
ahmedqat <ahmedlabeedqat@gmail.com>
2023-09-27 12:22:34 +0800

    quads

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.structuralEqualityPolicy
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +39,79 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    //TextQuad(text_title = stringResource(id = R.string.text_title), text_info = stringResource(id = R.string.text_info) )
+                    Starter()
                 }
             }
         }
     }
+}
+
+@Composable
+fun TextQuad(text_title: String, text_info: String, bg: Color,modifier: Modifier = Modifier){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(16.dp)
+            .background(bg)
+            .fillMaxSize()
+             ){
+        Text(
+            text = text_title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+
+
+        )
+        Text(
+            text = text_info,
+            textAlign = TextAlign.Justify
+
+
+        )
+    }
+    
+}
+
+@Composable
+fun Starter(){
+    Column(Modifier.fillMaxWidth()) {
+
+        Row (Modifier.weight(1f)){
+                TextQuad(text_title = stringResource(id = R.string.text_title),
+                    text_info = stringResource(id = R.string.text_info),
+                    bg = Color(0xFFEADDFF),
+                    modifier = Modifier.weight(1f))
+                TextQuad(
+                    text_title = stringResource(id = R.string.image_title),
+                    text_info = stringResource(id = R.string.image_info),
+                    bg = Color(0xFFD0BCFF),
+                    modifier = Modifier.weight(1f)
+                )
+
+        }
+        Row (Modifier.weight(1f)){
+            TextQuad(
+                text_title = stringResource(id = R.string.column_title),
+                text_info = stringResource(id = R.string.column_info),
+                bg = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            TextQuad(text_title = stringResource(id = R.string.row_title),
+                text_info = stringResource(id = R.string.row_info),
+                bg = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f))
+            
+        }
+
+    }
+
+
+
+
+
 }
 
 
@@ -35,6 +120,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     ComposeQuadrantTheme {
-        
+        Starter()
     }
 }
